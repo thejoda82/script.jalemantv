ae0965476cdda910f8a79e6fc4e5274510edbdd2
thejoda82 <thejoda82@users.noreply.github.com>
2018-04-01 08:06:25 -0500

    Add files via upload

---
+++ %%3381.py
@@ -0,0 +1,7 @@
+import urllib, urllib2, sys, re, os, random, unicodedata, cookielib, shutil, zipfile, xbmcvfs, time
+import xbmc, xbmcgui, xbmcplugin, xbmcaddon, base64, gui
+
+ADDON = xbmcaddon.Addon(id='script.jalemantv')
+Home = 'special://home/addons/script.jalemantv/'
+
+
